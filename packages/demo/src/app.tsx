1a669f0942d90ec3c49b3e7d3ad71cb6f34f405d
trash-and-fire <69399722+trash-and-fire@users.noreply.github.com>
2022-05-23 01:37:57 +0300

    update docs

--- packages/demo/src/app.tsx
+++ packages/demo/src/app.tsx
@@ -1,15 +1,16 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom';
 import {Gallery} from './gallery';
 import {Terminal} from './terminal';
 
 function App() {
+    const Router = process.env.NODE_ENV === 'production' ? HashRouter : BrowserRouter;
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Router basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<Gallery/>}/>
                 <Route path="/terminal/" element={<Terminal/>}/>
             </Routes>
-        </BrowserRouter>
+        </Router>
     );
 }
 
