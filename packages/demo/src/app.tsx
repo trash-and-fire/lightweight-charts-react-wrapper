657bd609590182f073b4d5eac081727a35d17cd9
trash-and-fire <69399722+trash-and-fire@users.noreply.github.com>
2022-05-23 12:11:19 +0300

    Revert "update docs"
    This reverts commit 1a669f0942d90ec3c49b3e7d3ad71cb6f34f405d.

--- packages/demo/src/app.tsx
+++ packages/demo/src/app.tsx
@@ -1,16 +1,15 @@
-import {BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {Gallery} from './gallery';
 import {Terminal} from './terminal';
 
 function App() {
-    const Router = process.env.NODE_ENV === 'production' ? HashRouter : BrowserRouter;
     return (
-        <Router basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<Gallery/>}/>
                 <Route path="/terminal/" element={<Terminal/>}/>
             </Routes>
-        </Router>
+        </BrowserRouter>
     );
 }
 
