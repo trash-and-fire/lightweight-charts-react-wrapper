092aecd7f371e1e2d9d6426bcd8df9f5de5447f7
trash-and-fire <69399722+trash-and-fire@users.noreply.github.com>
2022-05-25 02:34:51 +0300

    apply series defaults

--- packages/lib/src/internal/utils.ts
+++ packages/lib/src/internal/utils.ts
@@ -5,6 +5,51 @@ export function ensure<T>(value: T | null | undefined): T {
     return value;
 }
 
+export function clone<T>(object: T): T {
+    const source = object as any;
+
+    if (source === null || typeof source !== 'object') {
+        return source;
+    }
+
+    const result: any = Array.isArray(source) ? [] : {};
+
+    for (let key in source) {
+        if (!source.hasOwnProperty(key)) {
+            continue;
+        }
+        const value = source[key];
+
+        if (value !== null && typeof value === 'object') {
+            result[key] = clone(value);
+        } else {
+            result[key] = value;
+        }
+    }
+
+    return result;
+}
+
+export function merge(dest: Record<string, any>, source: Record<string, any>): Record<string, any> {
+    for (const key in source) {
+        if (!source.hasOwnProperty(key)) {
+            continue
+        }
+        if (source[key] === undefined) {
+            continue;
+        }
+
+        if (dest[key] === undefined || typeof source[key] !== 'object') {
+            dest[key] = source[key];
+        } else {
+            merge(dest[key], source[key]);
+        }
+    }
+
+    return dest;
+}
+
+
 export interface ActionResult<T> {
     update(params: T): void;
     destroy(): void;
